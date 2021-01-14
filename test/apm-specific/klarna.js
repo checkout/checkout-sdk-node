8fe9e8a34cb57a7ddc80fd6719ae23e80f4306af
ioan.ghisoi95 <ionutghisoi95@gmail.com>
2021-01-14 17:32:16 +0200

    Update Tests

--- test/apm-specific/klarna.js
+++ test/apm-specific/klarna.js
@@ -65,6 +65,65 @@ describe('Klarna', () => {
         expect(session.session_id).to.equal('kcs_nogezkjesatuzj3g7ejlkzb66m');
     });
 
+    it('should create session in prod', async () => {
+        nock('https://api.checkout.com')
+            .post('/klarna/credit-sessions')
+            .reply(201, {
+                session_id: 'kcs_nogezkjesatuzj3g7ejlkzb66m',
+                client_token:
+                    'eyJhbGciOiJSUzI1NiIsImtpZCI6IjgyMzA1ZWJjLWI4MTEtMzYzNy1hYTRjLTY2ZWNhMTg3NGYzZCJ9.ewogICJzZXNzaW9uX2lkIiA6ICI5OWE4M2I1Zi0yOTBiLTI4OGMtOTIwMC00MzBhMjY3NTBiZWUiLAogICJiYXNlX3VybCIgOiAiaHR0cHM6Ly9rbGFybmEtcGF5bWVudHMtZXUucGxheWdyb3VuZC5rbGFybmEuY29tL3BheW1lbnRzIiwKICAiZGVzaWduIiA6ICJrbGFybmEiLAogICJsYW5ndWFnZSIgOiAiZW4iLAogICJwdXJjaGFzZV9jb3VudHJ5IiA6ICJHQiIsCiAgInRyYWNlX2Zsb3ciIDogZmFsc2UsCiAgImVudmlyb25tZW50IiA6ICJwbGF5Z3JvdW5kIiwKICAibWVyY2hhbnRfbmFtZSIgOiAiUGxheWdyb3VuZCBEZW1vIE1lcmNoYW50IiwKICAic2Vzc2lvbl90eXBlIiA6ICJQQVlNRU5UUyIsCiAgImNsaWVudF9ldmVudF9iYXNlX3VybCIgOiAiaHR0cHM6Ly9ldS5wbGF5Z3JvdW5kLmtsYXJuYWV2dC5jb20iLAogICJleHBlcmltZW50cyIgOiBbIHsKICAgICJuYW1lIiA6ICJpbi1hcHAtc2RrLW5ldy1pbnRlcm5hbC1icm93c2VyIiwKICAgICJwYXJhbWV0ZXJzIiA6IHsKICAgICAgInZhcmlhdGVfaWQiIDogIm5ldy1pbnRlcm5hbC1icm93c2VyLWVuYWJsZSIKICAgIH0KICB9IF0KfQ.PuPoprscVa6y63tCMJvpurQcCm4-8ou0KTDerRWEY61Zz5d4-KIp-LCvUmU9WAtImigxNN-uFeS6VpFILj2D4rzV3cABli6lz4Rj9OmjX19BSiD8_GIuNkA0hSnE0KRk28AfjUpByb1WF2i_w5V_c6BUEtG-kGcA60YwDUHU9X92nO466Ox3X6NfPNOJAX1uwX8VLUdu7kCwoH5jYUqOn6f4KfMydxYA1XXr3HxvddJDoqPspeLwuFBxGYxPO721lpbYotRWNbYGJM4zLzs0g8_6vqeQ2IUf02HfwjebcwpO7MlVBrZ12L44VMDXHqZN3h2bu1vjpGSCwueEvU8RNg',
+                payment_method_categories: [
+                    {
+                        identifier: 'pay_later',
+                        name: 'Pay later in 14 days',
+                        asset_urls: {
+                            descriptive:
+                                'https://x.klarnacdn.net/payment-method/assets/badges/generic/klarna.svg',
+                            standard:
+                                'https://x.klarnacdn.net/payment-method/assets/badges/generic/klarna.svg',
+                        },
+                    },
+                    {
+                        identifier: 'pay_over_time',
+                        name: 'Monthly financing',
+                        asset_urls: {
+                            descriptive:
+                                'https://x.klarnacdn.net/payment-method/assets/badges/generic/klarna.svg',
+                            standard:
+                                'https://x.klarnacdn.net/payment-method/assets/badges/generic/klarna.svg',
+                        },
+                    },
+                ],
+                _links: {
+                    self: {
+                        href:
+                            'https://api.sandbox.checkout.com/klarna-external/credit-sessions/kcs_nogezkjesatuzj3g7ejlkzb66m',
+                    },
+                },
+            });
+
+        const cko = new Checkout('sk_0b9b5db6-f223-49d0-b68f-f6643dd4f808');
+
+        const session = await cko.klarna.createSession({
+            purchase_country: 'GB',
+            currency: 'GBP',
+            locale: 'en-GB',
+            amount: 1000,
+            tax_amount: 1,
+            products: [
+                {
+                    name: 'Brown leather belt',
+                    quantity: 1,
+                    unit_price: 1000,
+                    tax_rate: 0,
+                    total_amount: 1000,
+                    total_tax_amount: 0,
+                },
+            ],
+        });
+        expect(session.session_id).to.equal('kcs_nogezkjesatuzj3g7ejlkzb66m');
+    });
+
     it('should throw ValidationError when trying to create a session', async () => {
         nock('https://api.sandbox.checkout.com')
             .post('/klarna-external/credit-sessions')
@@ -130,6 +189,43 @@ describe('Klarna', () => {
             });
 
         const cko = new Checkout(SK);
+
+        const session = await cko.klarna.getSession('kcs_nogezkjesatuzj3g7ejlkzb66m');
+        expect(session.purchase_country).to.equal('gb');
+    });
+
+    it('should get live session', async () => {
+        nock('https://api.checkout.com')
+            .get('/klarna/credit-sessions/kcs_nogezkjesatuzj3g7ejlkzb66m')
+            .reply(200, {
+                client_token:
+                    'eyJhbGciOiJSUzI1NiIsImtpZCI6IjgyMzA1ZWJjLWI4MTEtMzYzNy1hYTRjLTY2ZWNhMTg3NGYzZCJ9.ewogICJzZXNzaW9uX2lkIiA6ICI5OWE4M2I1Zi0yOTBiLTI4OGMtOTIwMC00MzBhMjY3NTBiZWUiLAogICJiYXNlX3VybCIgOiAiaHR0cHM6Ly9rbGFybmEtcGF5bWVudHMtZXUucGxheWdyb3VuZC5rbGFybmEuY29tL3BheW1lbnRzIiwKICAiZGVzaWduIiA6ICJrbGFybmEiLAogICJsYW5ndWFnZSIgOiAiZW4iLAogICJwdXJjaGFzZV9jb3VudHJ5IiA6ICJHQiIsCiAgInRyYWNlX2Zsb3ciIDogZmFsc2UsCiAgImVudmlyb25tZW50IiA6ICJwbGF5Z3JvdW5kIiwKICAibWVyY2hhbnRfbmFtZSIgOiAiUGxheWdyb3VuZCBEZW1vIE1lcmNoYW50IiwKICAic2Vzc2lvbl90eXBlIiA6ICJQQVlNRU5UUyIsCiAgImNsaWVudF9ldmVudF9iYXNlX3VybCIgOiAiaHR0cHM6Ly9ldS5wbGF5Z3JvdW5kLmtsYXJuYWV2dC5jb20iLAogICJleHBlcmltZW50cyIgOiBbIHsKICAgICJuYW1lIiA6ICJpbi1hcHAtc2RrLW5ldy1pbnRlcm5hbC1icm93c2VyIiwKICAgICJwYXJhbWV0ZXJzIiA6IHsKICAgICAgInZhcmlhdGVfaWQiIDogIm5ldy1pbnRlcm5hbC1icm93c2VyLWVuYWJsZSIKICAgIH0KICB9IF0KfQ.PuPoprscVa6y63tCMJvpurQcCm4-8ou0KTDerRWEY61Zz5d4-KIp-LCvUmU9WAtImigxNN-uFeS6VpFILj2D4rzV3cABli6lz4Rj9OmjX19BSiD8_GIuNkA0hSnE0KRk28AfjUpByb1WF2i_w5V_c6BUEtG-kGcA60YwDUHU9X92nO466Ox3X6NfPNOJAX1uwX8VLUdu7kCwoH5jYUqOn6f4KfMydxYA1XXr3HxvddJDoqPspeLwuFBxGYxPO721lpbYotRWNbYGJM4zLzs0g8_6vqeQ2IUf02HfwjebcwpO7MlVBrZ12L44VMDXHqZN3h2bu1vjpGSCwueEvU8RNg',
+                _links: {
+                    self: {
+                        href:
+                            'https://api.sandbox.checkout.com/klarna-external/credit-sessions/kcs_nogezkjesatuzj3g7ejlkzb66m',
+                    },
+                },
+                purchase_country: 'gb',
+                currency: 'gbp',
+                locale: 'en-GB',
+                amount: 1000,
+                tax_amount: 1,
+                products: [
+                    {
+                        type: 'physical',
+                        name: 'Brown leather belt',
+                        quantity: 1,
+                        unit_price: 1000,
+                        tax_rate: 0,
+                        total_amount: 1000,
+                        total_discount_amount: 0,
+                        total_tax_amount: 0,
+                    },
+                ],
+            });
+
+        const cko = new Checkout('sk_0b9b5db6-f223-49d0-b68f-f6643dd4f808');
 
         const session = await cko.klarna.getSession('kcs_nogezkjesatuzj3g7ejlkzb66m');
         expect(session.purchase_country).to.equal('gb');
--- test/apm-specific/sepa.js
+++ test/apm-specific/sepa.js
@@ -42,6 +42,42 @@ describe('Sepa', () => {
         expect(sepa.mandate_reference).to.equal('Z10001205026454');
     });
 
+    it('should get live mandate', async () => {
+        nock('https://api.checkout.com')
+            .get('/sepa/mandates/src_juu42y3bte2ezfkymi7vmc7jcm')
+            .reply(200, {
+                mandate_reference: 'Z10001205026454',
+                customer_id: 'cus_w3kii25gnzmuphtb3ublwt52au',
+                first_name: 'Sophie',
+                last_name: 'Bonneville',
+                address_line1: '101 Avenue de Gaulle',
+                city: 'Paris',
+                zip: '75013',
+                country: 'FR',
+                masked_account_iban: '******************7893',
+                account_currency_code: 'EUR',
+                account_country_code: 'DE',
+                mandate_state: 'Active',
+                billing_descriptor: 'Thanks for shopping! - Mandate: Z10001205026454',
+                mandate_type: 'Single',
+                _links: {
+                    self: {
+                        href:
+                            'https://nginxtest.ckotech.co/sepa-external/mandates/src_juu42y3bte2ezfkymi7vmc7jcm',
+                    },
+                    'sepa:mandate-cancel': {
+                        href:
+                            'https://nginxtest.ckotech.co/sepa-external/mandates/src_juu42y3bte2ezfkymi7vmc7jcm/cancel',
+                    },
+                },
+            });
+
+        const cko = new Checkout('sk_0b9b5db6-f223-49d0-b68f-f6643dd4f808');
+
+        const sepa = await cko.sepa.getMandate('src_juu42y3bte2ezfkymi7vmc7jcm');
+        expect(sepa.mandate_reference).to.equal('Z10001205026454');
+    });
+
     it('should throw NotFFound error when trying to get mandate', async () => {
         nock('https://api.sandbox.checkout.com')
             .get('/sepa-external/mandates/src_juu42y3bte2ezfkymi7vmc7111')
@@ -73,6 +109,27 @@ describe('Sepa', () => {
             });
 
         const cko = new Checkout(SK);
+
+        const sepa = await cko.sepa.cancelMandate('src_htc2tova4cauldk7r47czuudji');
+    });
+
+    it('should cancel live mandate', async () => {
+        nock('https://api.checkout.com')
+            .post('/sepa/mandates/src_htc2tova4cauldk7r47czuudji/cancel')
+            .reply(200, {
+                _links: {
+                    self: {
+                        href:
+                            'https://api.sandbox.checkout.com/sepa-external/mandates/src_htc2tova4cauldk7r47czuudji/cancel',
+                    },
+                    'sepa:mandate-get': {
+                        href:
+                            'https://api.sandbox.checkout.com/sepa-external/mandates/src_htc2tova4cauldk7r47czuudji',
+                    },
+                },
+            });
+
+        const cko = new Checkout('sk_0b9b5db6-f223-49d0-b68f-f6643dd4f808');
 
         const sepa = await cko.sepa.cancelMandate('src_htc2tova4cauldk7r47czuudji');
     });
@@ -127,6 +184,42 @@ describe('Sepa', () => {
         expect(sepa.mandate_reference).to.equal('Z10001205026454');
     });
 
+    it('should get live mandate via PPRO', async () => {
+        nock('https://api.checkout.com')
+            .get('/ppro/sepa/mandates/src_juu42y3bte2ezfkymi7vmc7jcm')
+            .reply(200, {
+                mandate_reference: 'Z10001205026454',
+                customer_id: 'cus_w3kii25gnzmuphtb3ublwt52au',
+                first_name: 'Sophie',
+                last_name: 'Bonneville',
+                address_line1: '101 Avenue de Gaulle',
+                city: 'Paris',
+                zip: '75013',
+                country: 'FR',
+                masked_account_iban: '******************7893',
+                account_currency_code: 'EUR',
+                account_country_code: 'DE',
+                mandate_state: 'Active',
+                billing_descriptor: 'Thanks for shopping! - Mandate: Z10001205026454',
+                mandate_type: 'Single',
+                _links: {
+                    self: {
+                        href:
+                            'https://nginxtest.ckotech.co/sepa-external/mandates/src_juu42y3bte2ezfkymi7vmc7jcm',
+                    },
+                    'sepa:mandate-cancel': {
+                        href:
+                            'https://nginxtest.ckotech.co/sepa-external/mandates/src_juu42y3bte2ezfkymi7vmc7jcm/cancel',
+                    },
+                },
+            });
+
+        const cko = new Checkout('sk_0b9b5db6-f223-49d0-b68f-f6643dd4f808');
+
+        const sepa = await cko.sepa.getPPROMandate('src_juu42y3bte2ezfkymi7vmc7jcm');
+        expect(sepa.mandate_reference).to.equal('Z10001205026454');
+    });
+
     it('should throw NotFound error when trying to get mandate via PPRO', async () => {
         nock('https://api.sandbox.checkout.com')
             .get('/ppro/sepa-external/mandates/src_juu42y3bte2ezfkymi7vmc7111')
@@ -158,6 +251,27 @@ describe('Sepa', () => {
             });
 
         const cko = new Checkout(SK);
+
+        const sepa = await cko.sepa.cancelPPROMandate('src_htc2tova4cauldk7r47czuudji');
+    });
+
+    it('should cancel live mandate via PPRO', async () => {
+        nock('https://api.checkout.com')
+            .post('/ppro/sepa/mandates/src_htc2tova4cauldk7r47czuudji/cancel')
+            .reply(200, {
+                _links: {
+                    self: {
+                        href:
+                            'https://api.sandbox.checkout.com/sepa-external/mandates/src_htc2tova4cauldk7r47czuudji/cancel',
+                    },
+                    'sepa:mandate-get': {
+                        href:
+                            'https://api.sandbox.checkout.com/sepa-external/mandates/src_htc2tova4cauldk7r47czuudji',
+                    },
+                },
+            });
+
+        const cko = new Checkout('sk_0b9b5db6-f223-49d0-b68f-f6643dd4f808');
 
         const sepa = await cko.sepa.cancelPPROMandate('src_htc2tova4cauldk7r47czuudji');
     });
