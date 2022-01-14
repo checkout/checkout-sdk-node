2740bce2b9b0dd53830b3575584c714c009c23e9
Ming Jin <mingjinsoo@gmail.com>
2022-01-14 19:21:14 +0800

    update hosted payment page to get session status

--- src/api/hosted-payments/hosted-payments.js
+++ src/api/hosted-payments/hosted-payments.js
@@ -34,4 +34,25 @@ export default class HostedPayments {
             throw error;
         }
     }
+
+    /**
+     * Retrieve details about a specific Hosted Payment session using its ID returned when the link was created. In the response, you will see the status of the Hosted Payment session.
+     *
+     * @memberof HostedPayments
+     * @param {string} id
+     * @return {Promise<Object>} A promise to the Hosted Payment response.
+     */
+    async get(id) {
+        try {
+            const response = await http(fetch, this.config, {
+                method: 'get',
+                url: `${this.config.host}/hosted-payments/${id}`,
+                headers: { Authorization: this.config.sk },
+            });
+            return await response.json;
+        } catch (err) {
+            const error = await determineError(err);
+            throw error;
+        }
+    }
 }
