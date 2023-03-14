786cb70c7f188147d015bf4d30bfd93bd83414da
Martin Seco <mseco.93@gmail.com>
2023-03-14 13:24:35 -0300

    Adds support for Update instrument details in Platforms (CS2)

--- types/dist/api/platforms/platforms.d.ts
+++ types/dist/api/platforms/platforms.d.ts
@@ -4,9 +4,10 @@ export default class Platforms {
 
     uploadFile: (purpose: string, path: string) => Promise<Object>;
     onboardSubEntity: (body: Object) => Promise<Object>;
-    getPaymentInstrumentDetails: (entityId: string, id: string) => Promise<Object>;
     getSubEntityDetails: (id: string) => Promise<Object>;
     updateSubEntityDetails: (id: string, body: Object) => Promise<Object>;
+    getPaymentInstrumentDetails: (entityId: string, id: string) => Promise<Object>;
+    updatePaymentInstrumentDetails: (entityId: string, id: string, body: Object) => Promise<Object>;
     addPaymentInstrument: (id: string, body: Object) => Promise<Object>;
     queryPaymentInstruments: (id: string, status?: string) => Promise<Object>;
     retrieveSubEntityPayoutSchedule: (id: string) => Promise<Object>;
