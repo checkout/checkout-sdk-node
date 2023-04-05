65cfabd980655ac452f4b7f3fcacf7c0e21feb6e
Martin Seco <mseco.93@gmail.com>
2023-04-05 10:33:24 -0300

    Add support for Forex (FX) rates API (CS2)

--- types/dist/api/forex/forex.d.ts
+++ types/dist/api/forex/forex.d.ts
@@ -1,6 +1,8 @@
 import { config } from '../../Checkout';
+
 export default class Forex {
     constructor(config: config);
 
     request: (body: Object) => Promise<Object>;
+    getRates: (body: Object) => Promise<Object>;
 }
