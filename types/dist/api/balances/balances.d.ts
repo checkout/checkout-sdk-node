4399a8a6815d5ade9645640509556b36223460b4
6aKa <Aleksandr.Pavliutin@gmail.com>
2023-04-14 18:07:21 +0300

    Update balances.d.ts

--- types/dist/api/balances/balances.d.ts
+++ types/dist/api/balances/balances.d.ts
@@ -2,5 +2,5 @@ import { config } from '../../Checkout';
 export default class Balances {
     constructor(config: config);
 
-    retrieve: (id: string) => Promise<any>;
+    retrieve: (id: string, currency?: string) => Promise<any>;
 }
