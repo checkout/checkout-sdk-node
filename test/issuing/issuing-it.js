bcc33e2c90a074298723b0e7dd25b431a5897e5b
Martin Seco <mseco.93@gmail.com>
2023-04-14 13:06:36 -0300

    Adds support for Card operations (CS2)

--- test/issuing/issuing-it.js
+++ test/issuing/issuing-it.js
@@ -1,10 +1,17 @@
 import {expect} from "chai";
-import {NotFoundError, ValidationError} from "../../src/services/errors";
+import { determineError, NotFoundError, ValidationError } from "../../src/services/errors";
 
-const {beforeEach, cko_issuing} = require('../fixtures');
+const {afterEach, cko_issuing} = require('../fixtures');
 
-describe('Issuing [integration]', () => {
+describe('Integration::Issuing', () => {
     describe('Cardholder', () => {
+
+        let cardholder;
+
+        before(async () => {
+            cardholder = await createCardholder()
+        })
+
         it('should create a cardholder', async () => {
             const cardholderResponse = await cko_issuing.issuing.createCardholder({
                 type: "individual",
@@ -90,8 +97,6 @@ describe('Issuing [integration]', () => {
         });
 
         it('should get a cardholder', async () => {
-            const cardholder = await createCardholder()
-
             const cardholderResponse = await cko_issuing.issuing.getCardholder(cardholder.id);
 
             expect(cardholderResponse.id).to.equal(cardholder.id);
@@ -109,10 +114,6 @@ describe('Issuing [integration]', () => {
         });
 
         it('should get a cardholders cards', async () => {
-            const cardholder = await createCardholder()
-
-            // TODO create cards for cardholder
-
             const cardsResponse = await cko_issuing.issuing.getCardholderCards(cardholder.id);
 
             expect(cardsResponse.cards).to.not.be.null
@@ -128,6 +129,170 @@ describe('Issuing [integration]', () => {
             } catch (err) {
                 expect(err).to.be.instanceOf(NotFoundError);
             }
+        });
+    });
+
+    describe('Cards', () => {
+
+        let cardholder;
+        let card;
+
+        before(async () => {
+            cardholder = await createCardholder()
+            card = await createCard(cardholder)
+        })
+
+        it('should create a virtual card', async () => {
+            const cardResponse = await cko_issuing.issuing.createCard({
+                type: "virtual",
+                cardholder_id: cardholder.id,
+                lifetime: {
+                    unit: "Months",
+                    value: 6
+                },
+                reference: "X-123456-N11",
+                card_product_id: "pro_2ebzpnw3wvcefnu7fqglqmg56m",
+                display_name: "JOHN KENNEDY",
+                is_single_use: false,
+                activate_card: false
+            })
+
+            expect(cardResponse.id).to.not.be.null
+            expect(cardResponse.display_name).to.equal("JOHN KENNEDY")
+            expect(cardResponse.reference).to.equal("X-123456-N11")
+        });
+
+        it('should throw ValidationError when creating a card with invalid data', async () => {
+            try {
+                await cko_issuing.issuing.createCard({
+                    type: "physical",
+                    cardholder_id: cardholder.id,
+                    lifetime: {
+                        unit: "Months",
+                        value: 6
+                    },
+                    reference: "X-123456-N11",
+                    card_product_id: "pro_2ebzpnw3wvcefnu7fqglqmg56m",
+                    display_name: "JOHN KENNEDY",
+                    activate_card: false
+                });
+            } catch (err) {
+                expect(err).to.be.instanceOf(ValidationError);
+            }
+        });
+
+        it('should get card', async () => {
+            const cardResponse = await cko_issuing.issuing.getCardDetails(card.id)
+
+            expect(cardResponse.id).to.equal(card.id)
+            expect(cardResponse.cardholder_id).to.equal(cardholder.id)
+            expect(cardResponse.card_product_id).to.equal("pro_2ebzpnw3wvcefnu7fqglqmg56m")
+            expect(cardResponse.reference).to.equal("X-123456-N11")
+        });
+
+        it('should throw NotFoundError when getting an unexistant card', async () => {
+            try {
+                await cko_issuing.issuing.createCard({
+                    type: "physical",
+                    cardholder_id: cardholder.id,
+                    lifetime: {
+                        unit: "Months",
+                        value: 6
+                    },
+                    reference: "X-123456-N11",
+                    card_product_id: "pro_2ebzpnw3wvcefnu7fqglqmg56m",
+                    display_name: "JOHN KENNEDY",
+                    activate_card: false
+                });
+            } catch (err) {
+                expect(err).to.be.instanceOf(ValidationError);
+            }
+        });
+
+
+        it.skip('should enroll card into 3DS', async () => {
+            const enrollResponse = await cko_issuing.issuing.enrollThreeDS(card.id, {
+                password: "Xtui43FvfiZ",
+                    locale: "en-US",
+                    phone_number: {
+                    country_code: "+1",
+                        number: "415 555 2671"
+                }
+            })
+
+            expect(enrollResponse).to.not.be.null
+        });
+
+        it.skip('should update enrollment into 3DS', async () => {
+            const enrollResponse = await cko_issuing.issuing.updateThreeDS(card.id, {
+                security_pair: {
+                    question: "Who are you?",
+                    answer: "Bond. James Bond."
+                },
+                password: "Xtui43FvfiZ",
+                locale: "en-US",
+                phone_number: {
+                    country_code: "+1",
+                    number: "415 555 2671"
+                }
+            })
+
+            expect(enrollResponse).to.not.be.null
+        });
+
+        it.skip('should get enrollment into 3DS', async () => {
+            const enrollmentResponse = await cko_issuing.issuing.getThreeDSDetails(card.id)
+
+            expect(enrollmentResponse).to.not.be.null
+            expect(enrollmentResponse.locale).to.equal("en-US");
+            expect(enrollmentResponse.phone_number.country_code).to.equal("+1")
+            expect(enrollmentResponse.phone_number.number).to.equal("415 555 2671")
+        });
+
+        it('should activate a card', async () => {
+            const activationResponse = await cko_issuing.issuing.activateCard(card.id)
+
+            expect(activationResponse).to.not.be.null
+
+            const cardResponse = await cko_issuing.issuing.getCardDetails(card.id)
+
+            expect(cardResponse.id).to.equal(card.id)
+            expect(cardResponse.status).to.equal("active")
+        });
+
+        it('should get card credentials', async () => {
+            const credentialsResponse = await cko_issuing.issuing.getCardCredentials(card.id, {
+                credentials: "number, cvc2"
+            })
+
+            expect(credentialsResponse.number).to.not.be.null
+            expect(credentialsResponse.cvc2).to.not.be.null
+        });
+
+        it('should suspend a card', async () => {
+            const suspendedResponse = await cko_issuing.issuing.suspendCard(card.id, {
+                reason: "suspected_lost"
+            })
+
+            expect(suspendedResponse).to.not.be.null
+
+            const cardResponse = await cko_issuing.issuing.getCardDetails(card.id)
+
+            expect(cardResponse.id).to.equal(card.id)
+            expect(cardResponse.status).to.equal("suspended")
+        });
+
+        it('should revoke a card', async () => {
+            const suspendedResponse = await cko_issuing.issuing.revokeCard(card.id, {
+                reason: "reported_lost"
+            })
+
+            expect(suspendedResponse).to.not.be.null
+
+            const cardResponse = await cko_issuing.issuing.getCardDetails(card.id)
+
+            expect(cardResponse.id).to.equal(card.id)
+            expect(cardResponse.status).to.equal("revoked")
         });
     });
 });
@@ -167,5 +332,21 @@ const createCardholder = async () => {
             front_document_id: "file_6lbss42ezvoufcb2beo76rvwly",
             back_document_id: "file_aaz5pemp6326zbuvevp6qroqu4"
         }
+    });
+}
+
+const createCard = async (cardholder) => {
+    return await cko_issuing.issuing.createCard({
+        type: "virtual",
+        cardholder_id: cardholder.id,
+        lifetime: {
+            unit: "Months",
+            value: 6
+        },
+        reference: "X-123456-N11",
+        card_product_id: "pro_2ebzpnw3wvcefnu7fqglqmg56m",
+        display_name: "JOHN KENNEDY",
+        is_single_use: false,
+        activate_card: false
     });
 }
--- test/issuing/issuing-unit.js
+++ test/issuing/issuing-unit.js
@@ -5,7 +5,7 @@ import {AuthenticationError, NotFoundError} from "../../src/services/errors";
 
 const SK = 'sk_test_0b9b5db6-f223-49d0-b68f-f6643dd4f808';
 
-describe('Issuing [unit]', () => {
+describe('Unit::Issuing', () => {
     describe('Cardholder', () => {
         it('should create a cardholder', async () => {
             nock('https://api.sandbox.checkout.com')
@@ -232,6 +232,384 @@ describe('Issuing [unit]', () => {
 
             try {
                 await cko.issuing.getCardholderCards('not_found');
+            } catch (err) {
+                expect(err).to.be.instanceOf(NotFoundError);
+            }
+        });
+    });
+
+    describe('Cards', () => {
+        it('should create card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards')
+                .reply(201, {
+                    id: "crd_fa6psq242dcd6fdn5gifcq1491",
+                    display_name: "JOHN KENNEDY",
+                    last_four: 1234,
+                    expiry_month: 5,
+                    expiry_year: 2025,
+                    billing_currency: "USD",
+                    issuing_country: "US",
+                    reference: "X-123456-N11",
+                    created_date: "2019-09-10T10:11:12Z",
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491"
+                        },
+                        credentials: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/credentials"
+                        },
+                        revoke: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/revoke"
+                        }
+                    }
+                })
+
+            const cko = new Checkout(SK);
+
+            const cardResponse = await cko.issuing.createCard({
+                type: "physical",
+                cardholder_id: "crh_d3ozhf43pcq2xbldn2g45qnb44",
+                lifetime: {
+                    unit: "Months",
+                    value: 6
+                },
+                reference: "X-123456-N11",
+                card_product_id: "pro_7syjig3jq3mezlc3vjrdpfitl4",
+                display_name: "JOHN KENNEDY",
+                shipping_instructions: {
+                    shipping_recipient: "john kennedy",
+                    shipping_address: {
+                        address_line1: "Checkout.com",
+                        address_line2: "90 Tottenham Court Road",
+                        city: "London",
+                        state: "London",
+                        zip: "W1T 4TJ",
+                        country: "GB"
+                    },
+                    additional_comment: "string"
+                },
+                activate_card: false
+            })
+
+            expect(cardResponse.id).to.equal("crd_fa6psq242dcd6fdn5gifcq1491")
+            expect(cardResponse.display_name).to.equal("JOHN KENNEDY")
+        });
+
+        it('should throw when creating a cardholder cards', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards')
+                .reply(401);
+
+            const cko = new Checkout('sk_123');
+
+            try {
+                await cko.issuing.createCard({});
+            } catch (err) {
+                expect(err).to.be.instanceOf(AuthenticationError);
+            }
+        });
+
+        it('should get a card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .get('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491')
+                .reply(200, {
+                    id: "crd_fa6psq242dcd6fdn5gifcq1491",
+                    cardholder_id: "crh_d3ozhf43pcq2xbldn2g45qnb44",
+                    card_product_id: "pro_7syjig3jq3mezlc3vjrdpfitl4",
+                    client_id: "cli_vkuhvk4vjn2edkps7dfsq6emqm",
+                    last_four: 1234,
+                    expiry_month: 5,
+                    expiry_year: 2025,
+                    status: "active",
+                    display_name: "JOHN KENNEDY",
+                    type: "physical",
+                    billing_currency: "USD",
+                    issuing_country: "US",
+                    reference: "X-123456-N11",
+                    created_date: "2021-09-09T19:41:39Z",
+                    last_modified_date: "2021-09-09T19:41:39Z",
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491"
+                        },
+                        credentials: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/credentials"
+                        },
+                        revoke: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/revoke"
+                        }
+                    }
+                });
+
+            const cko = new Checkout(SK);
+
+            const cardholderResponse = await cko.issuing.getCardDetails('crd_fa6psq242dcd6fdn5gifcq1491');
+
+            expect(cardholderResponse.id).to.equal("crd_fa6psq242dcd6fdn5gifcq1491");
+            expect(cardholderResponse.cardholder_id).to.equal("crh_d3ozhf43pcq2xbldn2g45qnb44")
+            expect(cardholderResponse.type).to.equal("physical")
+            expect(cardholderResponse.status).to.equal("active")
+        });
+
+        it('should throw when getting a card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .get('/issuing/cards/not_found')
+                .reply(404);
+
+            const cko = new Checkout('sk_123');
+
+            try {
+                await cko.issuing.getCardDetails('not_found');
+            } catch (err) {
+                expect(err).to.be.instanceOf(NotFoundError);
+            }
+        });
+
+        it('should enroll card into 3ds with password', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491/3ds-enrollment')
+                .reply(202, {
+                    created_date: "2019-09-10T10:11:12Z",
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/3ds-enrollment"
+                        }
+                    }
+                })
+
+            const cko = new Checkout(SK);
+
+            const enrollmentResponse = await cko.issuing.enrollThreeDS( "crd_fa6psq242dcd6fdn5gifcq1491", {
+                password: "Xtui43FvfiZ",
+                locale: "en-US",
+                phone_number: {
+                    country_code: "+1",
+                    number: "415 555 2671"
+                }
+            })
+
+            expect(enrollmentResponse.created_date).to.equal("2019-09-10T10:11:12Z")
+        });
+
+        it('should throw when enrolling into 3ds', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/not_found/3ds-enrollment')
+                .reply(404);
+
+            const cko = new Checkout('sk_123');
+
+            try {
+                await cko.issuing.enrollThreeDS("not_found", {});
+            } catch (err) {
+                expect(err).to.be.instanceOf(NotFoundError);
+            }
+        });
+
+        it('should update 3ds enrollment', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .patch('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491/3ds-enrollment')
+                .reply(202, {
+                    last_modified_date: "2019-09-11T10:11:12Z",
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/3ds-enrollment"
+                        }
+                    }
+                });
+
+            const cko = new Checkout(SK);
+
+            const enrollmentResponse = await cko.issuing.updateThreeDS("crd_fa6psq242dcd6fdn5gifcq1491", {
+                security_pair: {
+                    question: "Who are you?",
+                    answer: "Bond. James Bond."
+                },
+                password: "Xtui43FvfiZ",
+                locale: "en-US",
+                phone_number: {
+                    country_code: "+1",
+                    number: "415 555 2671"
+                }
+            });
+
+            expect(enrollmentResponse.last_modified_date).to.equal("2019-09-11T10:11:12Z")
+        });
+
+        it('should throw when updating enrollment into 3ds', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .patch('/issuing/cards/not_found/3ds-enrollment')
+                .reply(404);
+
+            const cko = new Checkout('sk_123');
+
+            try {
+                await cko.issuing.updateThreeDS("not_found", {});
+            } catch (err) {
+                expect(err).to.be.instanceOf(NotFoundError);
+            }
+        });
+
+        it('should get a card`s 3DS enrollment', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .get('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491/3ds-enrollment')
+                .reply(200, {
+                    locale: "en-US",
+                    phone_number: {
+                        country_code: "+1",
+                        number: "415 555 2671"
+                    },
+                    created_date: "2019-09-10T10:11:12Z",
+                    last_modified_date: "2019-09-11T10:11:12Z",
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/3ds-enrollment"
+                        }
+                    }
+                });
+
+            const cko = new Checkout(SK);
+
+            const enrollmentResponse = await cko.issuing.getThreeDSDetails('crd_fa6psq242dcd6fdn5gifcq1491');
+
+            expect(enrollmentResponse.locale).to.equal("en-US");
+            expect(enrollmentResponse.phone_number.country_code).to.equal("+1")
+            expect(enrollmentResponse.phone_number.number).to.equal("415 555 2671")
+        });
+
+        it('should throw when getting enrollment into 3ds', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .get('/issuing/cards/not_found/3ds-enrollment')
+                .reply(404);
+
+            const cko = new Checkout('sk_123');
+
+            try {
+                await cko.issuing.getThreeDSDetails("not_found");
+            } catch (err) {
+                expect(err).to.be.instanceOf(NotFoundError);
+            }
+        });
+
+        it('should activate a card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491/activate')
+                .reply(200, {
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491"
+                        },
+                        revoke: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/revoke"
+                        },
+                        suspend: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491/suspend"
+                        }
+                    }
+                })
+
+            const cko = new Checkout(SK);
+
+            const activationResponse = await cko.issuing.activateCard( "crd_fa6psq242dcd6fdn5gifcq1491")
+
+            expect(activationResponse).to.not.be.null
+        });
+
+        it('should throw when activating card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/not_found/activate')
+                .reply(404);
+
+            const cko = new Checkout(SK);
+
+            try {
+                await cko.issuing.activateCard("not_found");
+            } catch (err) {
+                expect(err).to.be.instanceOf(NotFoundError);
+            }
+        });
+
+        it('should get card credentials', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .get('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491/credentials?credentials=number,%20cvc2')
+                .reply(200, {
+                    number: 4242424242424242,
+                    cvc2: 604
+                })
+
+            const cko = new Checkout(SK);
+
+            const credentialsResponse = await cko.issuing.getCardCredentials( "crd_fa6psq242dcd6fdn5gifcq1491", {
+                credentials: "number, cvc2"
+            })
+
+            expect(credentialsResponse.number).to.equal(4242424242424242)
+            expect(credentialsResponse.cvc2).to.equal(604)
+        });
+
+        it('should revoke a card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491/revoke')
+                .reply(200, {
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491"
+                        }
+                    }
+                })
+
+            const cko = new Checkout(SK);
+
+            const revokeResponse = await cko.issuing.revokeCard( "crd_fa6psq242dcd6fdn5gifcq1491", {
+                reason: "reported_lost"
+            })
+
+            expect(revokeResponse).to.not.be.null
+        });
+
+        it('should throw when revoking a card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/not_found/revoke')
+                .reply(404);
+
+            const cko = new Checkout('sk_123');
+
+            try {
+                await cko.issuing.revokeCard("not_found", {});
+            } catch (err) {
+                expect(err).to.be.instanceOf(NotFoundError);
+            }
+        });
+
+        it('should suspend a card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/crd_fa6psq242dcd6fdn5gifcq1491/suspend')
+                .reply(200, {
+                    _links: {
+                        self: {
+                            href: "https://api.checkout.com/issuing/cards/crd_fa6psq42dcdd6fdn5gifcq1491"
+                        }
+                    }
+                })
+
+            const cko = new Checkout(SK);
+
+            const suspendedResponse = await cko.issuing.suspendCard( "crd_fa6psq242dcd6fdn5gifcq1491", {
+                reason: "suspected_lost"
+            })
+
+            expect(suspendedResponse).to.not.be.null
+        });
+
+        it('should throw when suspending a card', async () => {
+            nock('https://api.sandbox.checkout.com')
+                .post('/issuing/cards/not_found/suspend')
+                .reply(404);
+
+            const cko = new Checkout('sk_123');
+
+            try {
+                await cko.issuing.suspendCard("not_found", {});
             } catch (err) {
                 expect(err).to.be.instanceOf(NotFoundError);
             }
